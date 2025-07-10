802cd717e1e4ee874eb0cdb0bb22b5680fbdfe45
hagarabdelmotalb <114605750+hagarabdelmotalb@users.noreply.github.com>
2025-07-11 02:13:27 +0300

    Emplyees class

---
+++ assignment/HireDate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class HireDate
+{
+    private int day;
+    private int year;
+    private int month;
+
+    public HireDate(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int Day
+    {
+        get { return day; }
+        set
+        {
+            if (value > 0 && value <= 31)
+                day = value;
+            else
+                throw new ArgumentException("Invalid day");
+        }
+    }
+
+    public int Month
+    {
+        get { return month; }
+        set
+        {
+            if (month > 0 && month <= 12)
+                month = value;
+            else
+            {
+                throw new ArgumentException("invalid month");
+            }
+        }
+    }
+
+    public int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
+
+    public override string ToString()
+    {
+        return $"{Day}/ {month} / {year}";
+    }
+
+    public DateTime ToDateTime()
+    {
+        return new DateTime(Year, Month, Day);
+    }
+}
---
+++ assignment/ُEmployees.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+public enum SecurityLevel
+{
+    Guest,
+    Developer,
+    secretary,
+    DBA,
+    SecurityOfficer
+}
+
+public class Employees
+{
+    private int id;
+    private string name;
+    private SecurityLevel security_level;
+    private double salary;
+    private HireDate hiredate;
+    private char gender;
+
+    public Employees(int id, string name, SecurityLevel level, double salary, HireDate hiredate
+        , char gender)
+    {
+        this.id = id;
+        this.name = name;
+        this.security_level = level;
+        this.salary = salary;
+        this.gender = gender;
+        this.hiredate = hiredate;
+    }
+
+
+    public HireDate HireDate
+    {
+        get { return hiredate; }
+        set { hiredate = value; }
+    }
+
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+    public char Gender
+    {
+        get { return this.gender; }
+        set
+        {
+            if (value == 'M' || value == 'F')
+                this.gender = value;
+            else
+                throw new ArgumentException("gender must be M or F");
+        }
+    }
+
+    public override string ToString()
+    {
+        return
+        $"ID : {id}\n" +
+        $"Name : {name}\n" +
+        $"Salary : {string.Format("{0:C}", salary)}\n" +
+        $"Gender : {gender}\n" +
+        $"Security Level:{security_level} " +
+        $"Hire Date: {hiredate}"; ;
+    }
+}
